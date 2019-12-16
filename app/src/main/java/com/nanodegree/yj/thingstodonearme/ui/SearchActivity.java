f3db5aaf82fec96c8f36caa0a4bb209b832506bb
Yul Jae Lee <yuljlee9@gmail.com>
2019-12-15 23:39:55 -0800

    feat: Add current location

--- app/src/main/java/com/nanodegree/yj/thingstodonearme/ui/SearchActivity.java
+++ app/src/main/java/com/nanodegree/yj/thingstodonearme/ui/SearchActivity.java
@@ -1,10 +1,16 @@
 package com.nanodegree.yj.thingstodonearme.ui;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -18,6 +24,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nanodegree.yj.thingstodonearme.R;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,6 +43,8 @@ public class SearchActivity extends AppCompatActivity {
     @BindView(R.id.textView_city2) TextView mCity2;
     @BindView(R.id.textView_city3) TextView mCity3;
     @BindView(R.id.textView_city4) TextView mCity4;
+
+    @BindView(R.id.current_location_textView) TextView mCurrentLocation;
 
     private LocationManager locationManager;
     private String provider;
@@ -70,7 +82,7 @@ public class SearchActivity extends AppCompatActivity {
 //                        Toast.LENGTH_LONG).show();
 
                 String cityName = parent.getItemAtPosition(position).toString();
-                Intent intent=new Intent();
+                Intent intent = new Intent();
                 intent.putExtra("city_name", cityName);
                 setResult(RESULT_OK, intent);
 
@@ -78,6 +90,80 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+        mCurrentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1000);
+                } else {
+                    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    try {
+                        String city = hereLocation(location.getLatitude(), location.getLongitude());
+                        mAutoTextView.setText(city);
+                        setLocation(city);
+                        finish();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(SearchActivity.this, "Not Found!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    public void setLocation(String city) {
+        Intent intent = new Intent();
+        intent.putExtra("city_name", city);
+        setResult(RESULT_OK, intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1000: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    try {
+                        String city = hereLocation(location.getLatitude(), location.getLongitude());
+                        mAutoTextView.setText(city);
+                        setLocation(city);
+                        finish();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(SearchActivity.this, "Not Found!", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private String hereLocation(double lat, double lon) {
+        String cityName = "";
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = geocoder.getFromLocation(lat, lon, 10);
+            if (addresses.size() > 0) {
+                for (Address adr: addresses) {
+                    if (adr.getLocality() != null && adr.getLocality().length() > 0) {
+                        cityName = adr.getLocality();
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return cityName;
     }
 
     @OnClick({R.id.textView_city1, R.id.textView_city2, R.id.textView_city3, R.id.textView_city4})
