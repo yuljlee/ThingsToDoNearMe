634d048a02c7a2e91e954f930ed143f90502c02d
Yul Jae Lee <yuljlee9@gmail.com>
2020-01-01 18:19:49 -0800

    test: Banner Ad display test

--- app/src/main/java/com/nanodegree/yj/thingstodonearme/ui/DetailActivityFragment.java
+++ app/src/main/java/com/nanodegree/yj/thingstodonearme/ui/DetailActivityFragment.java
@@ -125,9 +125,9 @@ public class DetailActivityFragment extends Fragment implements
         View view = inflater.inflate(R.layout.fragment_detail, container, false);
         unbinder = ButterKnife.bind(this, view);
 
-//        MobileAds.initialize(getActivity(), "ca-app-pub-3940256099942544~3347511713");
-//        AdRequest adRequest = new AdRequest.Builder().build();
-//        mAdView.loadAd(adRequest);
+        MobileAds.initialize(getActivity(), "ca-app-pub-3940256099942544~3347511713");
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 //
 //        mAdView.setAdListener(new AdListener() {
 //            @Override
